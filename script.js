6d2c25833a2768aefb2805419c620f02d139f0fc
Jessica Gomez <jessicagomez@Jessicas-MBP.lan>
2023-07-27 22:21:29 -0700

    added functionality to Go and Clear buttons

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ var city = '';
 
 // use API to get weather alerts by location, fetch request
 function getAlertsByLocation() {
+    city = document.getElementById("search").value;
     fetch(`https://api.weatherbit.io/v2.0/alerts?city=${city}&key=0aec531cfccd4364841446fc91ca9602`)
       .then((resp) => resp.json())
       .then((respData) => {
@@ -20,35 +21,45 @@ function getAlertsByLocation() {
       .catch((error) => {
         console.error("Error occurred:", error);
       });
+      document.getElementById("search").value = "";
   }
 
-  // add alerts to page, Rico will ask tutor about adding if/else statements for # of alerts
+  // add alerts to page, Rico will ask tutor about adding if/then statements for # of alerts
       function addAlertsToPage(data){
           var cityAlerts = document.createElement('div')
           cityAlerts.classList.add('weather');
 
           cityAlerts.innerHTML = `
-          <h4>${data.alerts[0].title}</h4>
+          <h4>${data.city_name}</h4>
+          <p> country_code: ${data.country_code}</p>
+          <p> lat: ${data.lat}</p>
+          <p> lon: ${data.lon}</p>
+          <p> state_code: ${data.state_code}</p>
+          <p> timezone: ${data.timezone}</p>
           
-need to add if/else statements
-        // if(${data.alerts[0].title} === 0) {
-        //  cityAlerts.innerHTML = '
-        //  <h4>${data.alerts[0].title}</h4>
-        //  } else {
+          
+        
+          `;
+/*        weather.innerHTML = `
+            <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°F <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
+            <h4>${data.weather[0].main}</h4>
+            <h4>Humidity: ${data.main.humidity}</h4>
+            <h4>Wind Speed: ${data.wind.speed}</h4>
+            `;
+            */
 
-        //   }
-          
-        
-          `; 
-        
+
           main.innerHTML= "";
            main.append(cityAlerts);
       };
 
-// need to configure searchBtn and clearBtn
-      document.getElementById("searchBtn").addEventListener("click", getAlertsByLocation());
+      document.getElementById("searchBtn").addEventListener("click", getAlertsByLocation);
 
-     // click enter, results show
+      document.getElementById("clearBtn").addEventListener("click", () =>{
+        document.getElementById("search").value = "";
+      });
+
+     // click search button event
      form.on('submit',(event) =>{
       console.log("SUBMIT")
         event.preventDefault();
